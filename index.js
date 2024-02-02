47c2285ad6e1866e96120557a3d9611e5649999a
WampuZera <102239098+WampuZera@users.noreply.github.com>
2024-02-02 19:21:57 +0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Made with love, Best Price, Best Quality"];
 
 
 let currentIndex = 0;
